b32e91246fa02b5d648055c191a37ecb35f75f9d
Autch <autch@autch.net>
2016-08-12 04:42:31 +0900

    デバイスが開けない時の例外を修正

--- librdpc101.py
+++ librdpc101.py
@@ -3,7 +3,7 @@
 import hid
 
 RDPC101_VENDORID = 0x10c4
-RDPC101_PRODUCTID	= 0x818a
+RDPC101_PRODUCTID = 0x818a
 
 RDPC_STATE_INDEX_MA = 1
 RDPC_STATE_INDEX_SIGINTENSITY = 2
@@ -32,104 +32,111 @@ RFD_AM = 0
 RFD_FM = 1
 RFD_TV = 2
 
+
 def enumerate():
-  return hid.enumerate(RDPC101_VENDORID, RDPC101_PRODUCTID)
+    return hid.enumerate(RDPC101_VENDORID, RDPC101_PRODUCTID)
+
 
 class BandMap:
-  def __init__(self):
-    self.band_map = [
-      { 'band': RDPC_BAND_AM,  'min': 522, 'max': 1629, 'step': 9 },
-      { 'band': RDPC_BAND_FM,  'min': 7600, 'max': 9000, 'step': 10 },
-      { 'band': RDPC_BAND_FM,  'min': 9005, 'max': 10800, 'step': 5 }
-    ]
+    def __init__(self):
+        self.band_map = [
+            {'band': RDPC_BAND_AM, 'min': 522, 'max': 1629, 'step': 9},
+            {'band': RDPC_BAND_FM, 'min': 7600, 'max': 9000, 'step': 10},
+            {'band': RDPC_BAND_FM, 'min': 9005, 'max': 10800, 'step': 5}
+        ]
 
-  def get_band(self, freq):
-    for b in self.band_map:
-      if b['min'] <= freq and freq <= b['max']:
-        return b
-    return None
-  
-  def get_band_by_index(self, idx):
-    return self.band_map[idx]
+    def get_band(self, freq):
+        for b in self.band_map:
+            if b['min'] <= freq <= b['max']:
+                return b
+        return None
 
-  def get_band_name(self, freq):
-    band = self.get_band(freq)
-    if band['band'] == RDPC_BAND_FM:
-      return 'FM'
-    elif band['band'] == RDPC_BAND_AM:
-      return 'AM'
-    return '??'
+    def get_band_by_index(self, idx):
+        return self.band_map[idx]
 
-  def get_freq_format(self, freq):
-    band = self.get_band(freq)
-    if band['band'] == RDPC_BAND_FM:
-      return '%3d.%2.2d MHz' % (freq / 100, freq % 100)
-    elif band['band'] == RDPC_BAND_AM:
-      return '%6d kHz' % (freq)
-    return '---- Hz'
+    def get_band_name(self, freq):
+        band = self.get_band(freq)
+        if not band:
+            return '??'
+        if band['band'] == RDPC_BAND_FM:
+            return 'FM'
+        elif band['band'] == RDPC_BAND_AM:
+            return 'AM'
+        return '??'
+
+    def get_freq_format(self, freq):
+        band = self.get_band(freq)
+        if not band:
+            return '---- Hz'
+        if band['band'] == RDPC_BAND_FM:
+            return '%3d.%2.2d MHz' % (freq / 100, freq % 100)
+        elif band['band'] == RDPC_BAND_AM:
+            return '%6d kHz' % freq
+        return '---- Hz'
+
 
 class RDPC101:
-  def __init__(self, path = None):
-    self.hid_dev = hid.device()
-    if path:
-      self.hid_dev.open_path(path)
-    else:
-      self.hid_dev.open(RDPC101_VENDORID, RDPC101_PRODUCTID)
-    self.report_pkt = None
+    def __init__(self, path=None):
+        self.hid_dev = hid.device()
+        if path:
+            self.hid_dev.open_path(path)
+        else:
+            self.hid_dev.open(RDPC101_VENDORID, RDPC101_PRODUCTID)
+        self.report_pkt = None
 
-  def close(self):
-    self.hid_dev.close()
+    def close(self):
+        self.hid_dev.close()
 
-  def update_status(self):
-    self.report_pkt = self.hid_dev.read(1024)
+    def update_status(self):
+        self.report_pkt = self.hid_dev.read(1024)
 
-  def get_freq(self):
-    if not self.report_pkt:
-      self.update_status()
-    return ((self.report_pkt[RDPC_STATE_INDEX_FREQ_HI] << 8) |
-      self.report_pkt[RDPC_STATE_INDEX_FREQ_LO])
+    def get_freq(self):
+        if not self.report_pkt:
+            self.update_status()
+        return ((self.report_pkt[RDPC_STATE_INDEX_FREQ_HI] << 8) |
+                self.report_pkt[RDPC_STATE_INDEX_FREQ_LO])
 
-  def get_channels(self):
-    if not self.report_pkt:
-      self.update_status()
-    return self.report_pkt[RDPC_STATE_INDEX_MA]
+    def get_channels(self):
+        if not self.report_pkt:
+            self.update_status()
+        return self.report_pkt[RDPC_STATE_INDEX_MA]
 
-  def get_intensity(self):
-    if not self.report_pkt:
-      self.update_status()
-    return self.report_pkt[RDPC_STATE_INDEX_SIGINTENSITY]
+    def get_intensity(self):
+        if not self.report_pkt:
+            self.update_status()
+        return self.report_pkt[RDPC_STATE_INDEX_SIGINTENSITY]
 
-  def get_seeking(self):
-    self.update_status()
-    return (self.report_pkt[RDPC_STATE_INDEX_MA] & RDPC_MA_SEEKING_MASK) != 0
+    def get_seeking(self):
+        self.update_status()
+        return (self.report_pkt[RDPC_STATE_INDEX_MA] & RDPC_MA_SEEKING_MASK) != 0
 
-  def _send_feature(self, packet):
-    return self.hid_dev.send_feature_report(packet)
+    def _send_feature(self, packet):
+        return self.hid_dev.send_feature_report(packet)
 
-  # mono = 0x00, stereo = 0x01
-  def set_ma(self, ma):
-    return self._send_feature([RDPC_MA, ma, 0x00])
+    # mono = 0x00, stereo = 0x01
+    def set_ma(self, ma):
+        return self._send_feature([RDPC_MA, ma, 0x00])
 
-  # mute off = 0, mute on = 1
-  def set_mute(self, mute):
-    return self._send_feature([RDPC_MUTE, mute, 0x00])
+    # mute off = 0, mute on = 1
+    def set_mute(self, mute):
+        return self._send_feature([RDPC_MUTE, mute, 0x00])
 
-  # fm = 0x02, am = 0x80
-  def set_band(self, band):
-    return self._send_feature([RDPC_BAND, band, 0x02])
+    # fm = 0x02, am = 0x80
+    def set_band(self, band):
+        return self._send_feature([RDPC_BAND, band, 0x02])
 
-  # in kilohertz
-  def set_freq(self, freq):
-    return self._send_feature([RDPC_SETFREQ, (freq >> 8) & 0xff, freq & 0xff])
+    # in kilohertz
+    def set_freq(self, freq):
+        return self._send_feature([RDPC_SETFREQ, (freq >> 8) & 0xff, freq & 0xff])
 
-  # up = 0x01, down = 0x02
-  def set_seek(self, direction):
-    return self._send_feature([RDPC_SEEK, direction, 0x00])
+    # up = 0x01, down = 0x02
+    def set_seek(self, direction):
+        return self._send_feature([RDPC_SEEK, direction, 0x00])
 
-  def get_channel_format(self):
-    ch = self.get_channels()
-    if ch == RDPC_MA_MONO:
-      return 'Mono'
-    elif ch == RDPC_MA_STEREO:
-      return 'Stereo'
-    return '----'
+    def get_channel_format(self):
+        ch = self.get_channels()
+        if ch == RDPC_MA_MONO:
+            return 'Mono'
+        elif ch == RDPC_MA_STEREO:
+            return 'Stereo'
+        return '----'
--- rdpc101.py
+++ rdpc101.py
@@ -8,144 +8,153 @@ import librdpc101
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-d', '--device', dest='device', type=int, help='use DEVICEth device, shown in order of -l')
-ap.add_argument('-D', '--down', action='store_const', const=librdpc101.RDPC_SEEK_DOWN, dest='seek', help='seek downward')
+ap.add_argument('-D', '--down', action='store_const', const=librdpc101.RDPC_SEEK_DOWN, dest='seek',
+                help='seek downward')
 ap.add_argument('-U', '--up', action='store_const', const=librdpc101.RDPC_SEEK_UP, dest='seek', help='seek upward')
 ap.add_argument('-m', '--mono', action='store_const', const=False, dest='stereo')
 ap.add_argument('-s', '--stereo', action='store_const', const=True, dest='stereo')
 ap.add_argument('-l', '--list', action='store_true', help='list devices and its status then exit')
 ap.add_argument('-S', '--scan', dest='scan', help='scan stations in specified band')
-ap.add_argument('-x', '--expert', dest='expert', help='tune that freq exactly')
+ap.add_argument('-x', '--expert', dest='expert', action='store_true', help='tune that freq exactly')
 ap.add_argument('freq', nargs='?', default=None, help='frequency in kilohertz or megahertz')
 
 args = ap.parse_args()
 freq = None
 
 if args.list:
-  bm = librdpc101.BandMap()
-  devices = sorted(librdpc101.enumerate(), key=lambda x: x['path'])
-  i = 0
-  for dev in devices:
-    d = librdpc101.RDPC101(dev['path'])
-    try:
-      d.update_status()
-      f = d.get_freq()
-      print "device %d: %s %s %s %ddB" % (i, bm.get_band_name(f), bm.get_freq_format(f), 
-        d.get_channel_format(), d.get_intensity())
-    finally:
-      d.close()
-    i += 1
-  sys.exit(0) 
+    bm = librdpc101.BandMap()
+    devices = sorted(librdpc101.enumerate(), key=lambda x: x['path'])
+    i = 0
+    for dev in devices:
+        d = librdpc101.RDPC101(dev['path'])
+        try:
+            d.update_status()
+            f = d.get_freq()
+            print "device %d: %s %s %s %ddB" % (i, bm.get_band_name(f), bm.get_freq_format(f),
+                                                d.get_channel_format(), d.get_intensity())
+        finally:
+            d.close()
+        i += 1
+    sys.exit(0)
 
 if args.freq:
-  f_freq = 0.0
-  i_freq = 0
-  try:
-    f_freq = float(args.freq)
-  except ValueError, e:
-    print "freq must be numerals"
-    ap.print_help()
-    sys.exit(1)
-
-  i_freq = int(f_freq)
-  bm = librdpc101.BandMap()
-
-  band = bm.get_band(i_freq * 100.0)
-  if band and band['band'] == librdpc101.RDPC_BAND_FM:
-    if args.expert:
-      freq = int(f_freq * 100.0)
-    else:
-      freq = ((int(f_freq * 100.0) + band['step'] / 2) / band['step']) * band['step']
-    if args.stereo == None:
-      args.stereo = True
-  band = bm.get_band(i_freq)
-  if freq == None and band and band['band'] == librdpc101.RDPC_BAND_AM:
-    if args.expert:
-      freq = i_freq
-    else:
-      freq = ((i_freq + (band['step'] / 2)) / band['step']) * band['step']
-    args.stereo = False
-  
-  if freq == None:
-    print 'Frequency out of range'
-    ap.print_help()
-    sys.exit(1)
-
-try:
-  if args.device != None:
-    devices = sorted(librdpc101.enumerate(), key=lambda x: x['path'])
-    if args.device >= len(devices): 
-      print 'Device index out of range'
-      sys.exit(1)
-    nth = devices[args.device]
-    if not nth:
-      print 'Device index out of range'
-      sys.exit(1)
-    dev = librdpc101.RDPC101(nth['path'])
-  else:
-    dev = librdpc101.RDPC101()
-
-  def show_status(d):
-    bm = librdpc101.BandMap()
-    f = d.get_freq()
-    print "%s %s %s %ddB" % (bm.get_band_name(f), bm.get_freq_format(f), 
-      d.get_channel_format(), d.get_intensity())
-
-  if freq:
-    cur_freq = dev.get_freq()
-    cur_band = bm.get_band(cur_freq)
-    band = bm.get_band(freq)
-
-    if cur_band['band'] != band['band']:
-      dev.set_band(band['band']) 
-    if cur_freq != freq:
-      dev.set_freq(freq)
-  elif args.seek != None:
-    dev.set_mute(1)
-    dev.set_seek(args.seek)
-    while dev.get_seeking():
-      time.sleep(0.2)
-    dev.set_mute(0)
-    dev.update_status()
-    show_status(dev)
-  elif args.scan != None:
-    bandname = args.scan.lower()
-    bm = librdpc101.BandMap()
-
-    o_freq = dev.get_freq()
-    o_band = bm.get_band(o_freq)
-
-    if bandname[0] == 'a':
-      band = bm.get_band_by_index(librdpc101.RFD_AM)
-    elif bandname[0] == 'f':
-      band = bm.get_band_by_index(librdpc101.RFD_FM)
-    else:
-      print 'unsupported band'
-      ap.print_help()
-      sys.exit(1)
-
-    dev.set_mute(1)
+    f_freq = 0.0
+    i_freq = 0
     try:
-      dev.set_band(band['band'])
-      dev.set_freq(band['min'])
-      while dev.get_seeking():
-        time.sleep(0.2)
+        f_freq = float(args.freq)
+    except ValueError, e:
+        print "freq must be numerals"
+        ap.print_help()
+        sys.exit(1)
 
-      p_freq = band['min']
-      c_freq = dev.get_freq()
-      while c_freq < band['max']:
-        dev.set_seek(librdpc101.RDPC_SEEK_UP)
+    i_freq = int(f_freq)
+    bm = librdpc101.BandMap()
+
+    band = bm.get_band(i_freq * 100.0)
+    if band and band['band'] == librdpc101.RDPC_BAND_FM:
+        if args.expert:
+            freq = int(f_freq * 100.0)
+        else:
+            freq = ((int(f_freq * 100.0) + band['step'] / 2) / band['step']) * band['step']
+        if args.stereo is None:
+            args.stereo = True
+    band = bm.get_band(i_freq)
+    if freq is None and band and band['band'] == librdpc101.RDPC_BAND_AM:
+        if args.expert:
+            freq = i_freq
+        else:
+            freq = ((i_freq + (band['step'] / 2)) / band['step']) * band['step']
+        args.stereo = False
+
+    if freq is None:
+        print 'Frequency out of range'
+        ap.print_help()
+        sys.exit(1)
+
+dev = None
+try:
+    try:
+        if args.device is not None:
+            devices = sorted(librdpc101.enumerate(), key=lambda x: x['path'])
+            if args.device >= len(devices):
+                print 'Device index out of range'
+                sys.exit(1)
+            nth = devices[args.device]
+            if not nth:
+                print 'Device index out of range'
+                sys.exit(1)
+            dev = librdpc101.RDPC101(nth['path'])
+        else:
+            dev = librdpc101.RDPC101()
+    except IOError, e:
+        print "Unable to open device"
+        sys.exit(2)
+
+    def show_status(d):
+        bm = librdpc101.BandMap()
+        f = d.get_freq()
+        print "%s %s %s %ddB" % (bm.get_band_name(f), bm.get_freq_format(f),
+                                 d.get_channel_format(), d.get_intensity())
+
+
+    if freq:
+        bm = librdpc101.BandMap()
+        cur_freq = dev.get_freq()
+        cur_band = bm.get_band(cur_freq)
+        band = bm.get_band(freq)
+
+        if not cur_band or cur_band['band'] != band['band']:
+            dev.set_band(band['band'])
+        if cur_freq != freq:
+            dev.set_freq(freq)
+    elif args.seek is not None:
+        dev.set_mute(1)
+        dev.set_seek(args.seek)
         while dev.get_seeking():
-          time.sleep(0.2)
-        c_freq = dev.get_freq()
-        if p_freq != c_freq:
-          show_status(dev)
-          p_freq = c_freq
-    finally:
-      if band['band'] != o_band['band']:
-        dev.set_band(o_band['band'])
-      dev.set_freq(o_freq)
-      dev.set_mute(0)
-  if args.stereo:
-    dev.set_ma(librdpc101.RDPC_MA_STEREO if args.stereo else librdpc101.RDPC_MA_MONO)
+            time.sleep(0.2)
+        dev.set_mute(0)
+        dev.update_status()
+        show_status(dev)
+    elif args.scan is not None:
+        bandname = args.scan.lower()
+        bm = librdpc101.BandMap()
+
+        o_freq = dev.get_freq()
+        o_band = bm.get_band(o_freq)
+
+        if bandname[0] == 'a':
+            band = bm.get_band_by_index(librdpc101.RFD_AM)
+        elif bandname[0] == 'f':
+            band = bm.get_band_by_index(librdpc101.RFD_FM)
+        else:
+            print 'unsupported band'
+            ap.print_help()
+            sys.exit(1)
+
+        dev.set_mute(1)
+        try:
+            dev.set_band(band['band'])
+            dev.set_freq(band['min'])
+            while dev.get_seeking():
+                time.sleep(0.2)
+
+            p_freq = band['min']
+            c_freq = dev.get_freq()
+            while c_freq < band['max']:
+                dev.set_seek(librdpc101.RDPC_SEEK_UP)
+                while dev.get_seeking():
+                    time.sleep(0.2)
+                c_freq = dev.get_freq()
+                if p_freq != c_freq:
+                    show_status(dev)
+                    p_freq = c_freq
+        finally:
+            if not o_band or band['band'] != o_band['band']:
+                dev.set_band(o_band['band'])
+            dev.set_freq(o_freq)
+            dev.set_mute(0)
+    if args.stereo:
+        dev.set_ma(librdpc101.RDPC_MA_STEREO if args.stereo else librdpc101.RDPC_MA_MONO)
 finally:
-  dev.close()
+    if dev:
+        dev.close()
